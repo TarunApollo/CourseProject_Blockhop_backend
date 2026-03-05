a7831de34d5012dc6b1f363f0b96f59c8642e6eb
Chengyu Yu <nightnight2024@gmail.com>
2026-03-05 19:13:01 +0100

    fix: commented the class Level.java to pass the compilation. Co-authored-by: Chengyu Yu <chengyu.yu@usi.ch> Co-authored-by: Leon Jacopo Costa <leon.costa@usi.ch>

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Level.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Level.java
@@ -3,11 +3,11 @@ package ch.usi.inf.bsc.sa4.lab02spring.model;
 import java.util.HashMap;
 
 public class Level {
-    String title;
-    String description;
-    boolean published;
-    boolean exitDoorOpen;
-    ClearCondition clearCondition;
-    HashMap<Position,GameObject> objectLayer;
-    HashMap<Position,GroundObject> worldLayer;
+    // String title;
+    // String description;
+    // boolean published;
+    // boolean exitDoorOpen;
+    // ClearCondition clearCondition;
+    // HashMap<Position,GameObject> objectLayer;
+    // HashMap<Position,GroundObject> worldLayer;
 }
