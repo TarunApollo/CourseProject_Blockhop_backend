7326736983173ffbf8d6a89a59de1534db1ebe44
Chengyu Yu <nightnight2024@gmail.com>
2026-03-05 18:53:14 +0100

    created skeleton for domain model. Co-authored-by: Chengyu Yu <chengyu.yu@usi.ch> Co-authored-by: Leon Jacopo Costa <leon.costa@usi.ch>

---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Box.java
@@ -0,0 +1,7 @@
+package ch.usi.inf.bsc.sa4.lab02spring.model;
+
+public class Box extends Item{
+    
+}
+
+
---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Position.java
@@ -0,0 +1,10 @@
+package ch.usi.inf.bsc.sa4.lab02spring.model;
+
+public record Position(
+    int x,
+    int y
+) 
+{
+
+    
+}
