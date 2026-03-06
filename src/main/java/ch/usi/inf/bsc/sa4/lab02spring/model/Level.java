6e6488693d48d1e7a908865a9f6487901b1f647b
Tarun Deepak <tarundeepak2005@gmail.com>
2026-03-06 16:21:48 +0100

    Add setters to Level

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Level.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Level.java
@@ -11,22 +11,25 @@ import java.util.Map;
 public class Level {
     @Id
     private String id;
-    String title;
-    String description;
-    boolean exitDoorOpen;
-    boolean published;
-    ClearCondition clearCondition;
-    StartFlag startingItem;
-    ExitDoor door;
-    Map<String, Date> timesPlayed = new HashMap<>();
-    HashMap<Position, GameObject> objectLayer = new HashMap<>();
-    HashMap<Position, GroundObject> worldLayer = new HashMap<>();
+    private String title;
+    private String description;
+    private boolean exitDoorOpen;
+    private boolean published;
+    private ClearCondition clearCondition;
+    private StartFlag startingItem;
+    private ExitDoor door;
+    private Map<String, Date> timesPlayed = new HashMap<>();
+    private HashMap<Position, GameObject> objectLayer = new HashMap<>();
+    private HashMap<Position, GroundObject> worldLayer = new HashMap<>();
 
     public Level(String title, String description) {
         this.title = title;
         this.description = description;
         this.published = false;
         this.exitDoorOpen = false;
+    }
+    public boolean isPublished(){
+        return this.published;
     }
 
     public String getId() {
@@ -38,5 +41,12 @@ public class Level {
 
     public String getDescription() {
         return description;
+    }
+
+    public void setTitle(String newTitle){
+        this.title = newTitle;
+    }
+    public void setDescription(String newDescription){
+        this.description = newDescription;
     }
 }
