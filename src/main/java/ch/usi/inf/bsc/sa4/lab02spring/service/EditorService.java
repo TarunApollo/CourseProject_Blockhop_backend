6d290ea7ab317b2960173132f671302027ac72e9
kvn1351 <kvn1351@gmail.com>
2026-03-07 12:54:32 +0100

    #10: Implemented createLevel() in EditorService + respective DTO

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
@@ -3,6 +3,8 @@ package ch.usi.inf.bsc.sa4.lab02spring.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CreateLevelDTO;
+import ch.usi.inf.bsc.sa4.lab02spring.model.Level;
 import ch.usi.inf.bsc.sa4.lab02spring.repository.LevelRepository;
 import ch.usi.inf.bsc.sa4.lab02spring.repository.UserRepository;
 
@@ -14,5 +16,13 @@ public class EditorService {
     @Autowired
     public EditorService(LevelRepository levelRepository) {
         this.levelRepository = levelRepository;
+    }
+        public Level createLevel(String creatorId, CreateLevelDTO createLevelDTO) {
+        Level level = new Level(
+            createLevelDTO.title(),
+            createLevelDTO.description(),
+            creatorId
+        );
+        return levelRepository.save(level);
     }
 }
