995c67ccbac7eb545dd601a4510c8f6a94d953ae
Leon Jacopo Costa <leon.costa@usi.ch>
2026-03-08 10:16:28 +0100

    move level logic to Level* classes

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/LevelController.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/LevelController.java
@@ -1,23 +1,72 @@
 package ch.usi.inf.bsc.sa4.lab02spring.controller;
+import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CloneLevelDTO;
+import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CreateLevelDTO;
+import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.LevelDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 import ch.usi.inf.bsc.sa4.lab02spring.service.EditorService;
 import ch.usi.inf.bsc.sa4.lab02spring.service.LevelService;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/levels")
 public class LevelController {
   private final LevelService levelService;
-  private final EditorService editorService;
 
   @Autowired
   public LevelController(LevelService levelService, EditorService editorService) {
     this.levelService = levelService;
-    this.editorService = editorService;
   }
 
-  // TODO: add all GET methods here like get published levels and whatnot..
+  @PostMapping()
+  public ResponseEntity<LevelDTO> createLevel(Authentication authentication, @RequestBody CreateLevelDTO createLevelDTO) {
+    String userId = getUserIdFromAuth(authentication);
+    if (userId == null) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    }
+    return ResponseEntity.ok(new LevelDTO(this.levelService.createLevel(createLevelDTO, userId)));
+  }
 
+  @GetMapping()
+  public List<LevelDTO> getLevels() {
+    var levels = this.levelService.getAllLevels();
+    return levels.stream().map(LevelDTO::new).toList();
+  }
+
+  @PostMapping("/clone")
+  public ResponseEntity<LevelDTO> cloneLevel(Authentication authentication, @RequestBody CloneLevelDTO cloneLevelDTO) {
+    String userId = getUserIdFromAuth(authentication);
+    if (userId == null) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    }
+    return this.levelService.cloneLevel(cloneLevelDTO, userId)
+            .map(LevelDTO::new)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.status(HttpStatus.FORBIDDEN).build());
+  }
+
+  private String getUserIdFromAuth(Authentication authentication) {
+    if (authentication == null) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    } else {
+      Object principal = authentication.getPrincipal();
+      if (principal instanceof Jwt jwt) {
+        return jwt.getClaimAsString("sub");
+      } else if (principal instanceof OAuth2User oauth2User) {
+        if (oauth2User.getAttribute("sub") == null) {
+          throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        return oauth2User.getAttribute("sub");
+      }
+      throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    }
+  }
 }
--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
@@ -4,11 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CreateLevelDTO;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Level;
 import ch.usi.inf.bsc.sa4.lab02spring.repository.LevelRepository;
-import ch.usi.inf.bsc.sa4.lab02spring.repository.UserRepository;
+
 
 @Service
 public class EditorService {
@@ -18,15 +16,6 @@ public class EditorService {
     @Autowired
     public EditorService(LevelRepository levelRepository) {
         this.levelRepository = levelRepository;
-    }
-
-    public Level createLevel(String creatorId, CreateLevelDTO createLevelDTO) {
-        Level level = new Level(
-            createLevelDTO.title(),
-            createLevelDTO.description(),
-            creatorId
-        );
-        return levelRepository.save(level);
     }
 
     public Optional<Level> cloneLevel(String creatorId, String sourceLevelId) {
