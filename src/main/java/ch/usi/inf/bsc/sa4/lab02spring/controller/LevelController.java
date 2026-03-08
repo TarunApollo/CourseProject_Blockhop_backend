14c8e54a5f73bb9bc9912718712bba4a18a596cc
Leon Jacopo Costa <leon.costa@usi.ch>
2026-03-08 10:30:45 +0100

    added authutil class and moved getUserIdFromAuth method there for tidy repo

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/LevelController.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/LevelController.java
@@ -2,17 +2,16 @@ package ch.usi.inf.bsc.sa4.lab02spring.controller;
 import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CloneLevelDTO;
 import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CreateLevelDTO;
 import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.LevelDTO;
+
+import static ch.usi.inf.bsc.sa4.lab02spring.utils.AuthUtils.getUserIdFromAuth;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
-
 import ch.usi.inf.bsc.sa4.lab02spring.service.EditorService;
 import ch.usi.inf.bsc.sa4.lab02spring.service.LevelService;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,47 +25,37 @@ public class LevelController {
     this.levelService = levelService;
   }
 
+  /// Creates a new empty level and returns a level dto
+  /// TODO: javadoc!
+  /// @param authentication abstract token for authentication (either jwt or oauth2)
+  /// @param createLevelDTO dto containing the necessary information to create a brand-new level
+  /// @return a 200 if OK otherwise a 401 if the user is not authenticated
   @PostMapping()
   public ResponseEntity<LevelDTO> createLevel(Authentication authentication, @RequestBody CreateLevelDTO createLevelDTO) {
     String userId = getUserIdFromAuth(authentication);
-    if (userId == null) {
-      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    }
     return ResponseEntity.ok(new LevelDTO(this.levelService.createLevel(createLevelDTO, userId)));
   }
 
+  /// Return a list of the levels present in the collection
+  /// TODO: javadoc!
+  ///
+  /// @return list of levels
   @GetMapping()
   public List<LevelDTO> getLevels() {
     var levels = this.levelService.getAllLevels();
     return levels.stream().map(LevelDTO::new).toList();
   }
 
+  /// Copies the given level if present and if the user is the creator of such level
+  /// TODO: javadoc!
+  ///
+  /// @return a 200 if user is authenticated and levels exists a 404 if level doesn't exist a 401 if user not authenticated
   @PostMapping("/clone")
   public ResponseEntity<LevelDTO> cloneLevel(Authentication authentication, @RequestBody CloneLevelDTO cloneLevelDTO) {
     String userId = getUserIdFromAuth(authentication);
-    if (userId == null) {
-      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    }
     return this.levelService.cloneLevel(cloneLevelDTO, userId)
             .map(LevelDTO::new)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.status(HttpStatus.FORBIDDEN).build());
-  }
-
-  private String getUserIdFromAuth(Authentication authentication) {
-    if (authentication == null) {
-      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    } else {
-      Object principal = authentication.getPrincipal();
-      if (principal instanceof Jwt jwt) {
-        return jwt.getClaimAsString("sub");
-      } else if (principal instanceof OAuth2User oauth2User) {
-        if (oauth2User.getAttribute("sub") == null) {
-          throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-        }
-        return oauth2User.getAttribute("sub");
-      }
-      throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
-    }
   }
 }
---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/utils/AuthUtils.java
@@ -0,0 +1,31 @@
+package ch.usi.inf.bsc.sa4.lab02spring.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.server.ResponseStatusException;
+
+public final class AuthUtils {
+    ///
+    /// TODO: javadoc!
+    ///
+    ///
+    public static String getUserIdFromAuth(Authentication authentication) {
+        if (authentication == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        } else {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof Jwt jwt) {
+                return jwt.getClaimAsString("sub");
+            } else if (principal instanceof
+                    OAuth2User oauth2User) {
+                if (oauth2User.getAttribute("sub") == null) {
+                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+                }
+                return oauth2User.getAttribute("sub");
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        }
+    }
+}
