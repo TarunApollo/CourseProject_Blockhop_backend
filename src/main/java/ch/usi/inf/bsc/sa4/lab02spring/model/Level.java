f39a9f4708789329095607e56a9f41719afa4bfb
Leon Jacopo Costa <leon.costa@usi.ch>
2026-03-08 10:12:43 +0100

    Level service methods for creating, and cloning levels

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Level.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Level.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("NullAway.Init")
--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/LevelService.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/LevelService.java
@@ -1,19 +1,39 @@
 package ch.usi.inf.bsc.sa4.lab02spring.service;
 
+import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CloneLevelDTO;
+import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.CreateLevelDTO;
+import ch.usi.inf.bsc.sa4.lab02spring.model.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ch.usi.inf.bsc.sa4.lab02spring.repository.LevelRepository;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 @Service
 public class LevelService {
     
     private final LevelRepository levelRepository;
-    private final UserService userService;
 
     @Autowired
     public LevelService(LevelRepository levelRepository, UserService userService) {
         this.levelRepository = levelRepository;
-        this.userService = userService;
+    }
+
+    public Level createLevel(CreateLevelDTO createLevelDTO, String userId) {
+        Level level = new Level(createLevelDTO.title(), createLevelDTO.description(), userId);
+        return levelRepository.save(level);
+    }
+
+    public Optional<Level> cloneLevel(CloneLevelDTO cloneLevelDTO, String userId) {
+        Optional<Level> optLevel = this.levelRepository.findById(cloneLevelDTO.sourceLevelId());
+        return optLevel.filter((level) -> level.getCreatorId().equals(userId))
+                .map((level) -> this.levelRepository.save(level.cloneFor(userId)));
+    }
+
+    public List<Level> getAllLevels() {
+        return levelRepository.findAll();
     }
 }
