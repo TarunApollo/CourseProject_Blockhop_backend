15f54bba51f4482c57d7fba7b85050059e616c3f
kvn1351 <kvn1351@gmail.com>
2026-03-08 15:06:12 +0100

    #20: changed StartFlag to implement a copy constructor for deep copying

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/StartFlag.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/StartFlag.java
@@ -4,8 +4,16 @@ public class StartFlag extends Item {
 
     public StartFlag() {}
 
+    //
+    // Copy constructor for deepcopying a StartFlaf
+    // Preserves inherited fields from the parent class
+    //
+    public StartFlag(StartFlag s) {
+        super(s);
+    }
+
     @Override
     public StartFlag copy() {
-        return new StartFlag();
+        return new StartFlag(this);
     }
 }
