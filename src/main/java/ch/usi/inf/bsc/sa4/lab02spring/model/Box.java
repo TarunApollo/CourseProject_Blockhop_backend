2e1a83af52dc6d60b487b5ee32b5c764c9efc99e
Alessandro <alessandro.tognoni@usi.ch>
2026-03-09 16:57:44 +0100

    Added TypeAlias for every record which implements GameObject. The convention used for naming is snake case. For example record StartFlag becomes start_flag.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Box.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/Box.java
@@ -1,4 +1,7 @@
 package ch.usi.inf.bsc.sa4.lab02spring.model;
 
+import org.springframework.data.annotation.TypeAlias;
+
+@TypeAlias("box")
 public record Box(int gid, Position pos, BoxContentType content) implements Item {
 }
--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/StartFlag.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/StartFlag.java
@@ -1,4 +1,7 @@
 package ch.usi.inf.bsc.sa4.lab02spring.model;
 
+import org.springframework.data.annotation.TypeAlias;
+
+@TypeAlias("start_flag")
 public record StartFlag(int gid, Position pos) implements Item {
 }
