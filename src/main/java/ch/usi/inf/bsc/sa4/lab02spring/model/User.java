2bd8c2ebe74189f7f4af8ed5a0fedef38e87f83f
Theodoros <160426868+thsthomas@users.noreply.github.com>
2026-03-09 14:19:50 +0100

    added structures to keep track of the levels played and completed

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/User.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/User.java
@@ -4,17 +4,31 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Document(collection = "users")
 @SuppressWarnings("NullAway.Init")
 public class User {
     @Id
     private final String id; // switchEduId
     private final String name;
+    private final Set<String> completedLevelsIds;
+    private final Set<String> playedLevelsIds;
 
     @PersistenceCreator
+    public User(String id, String name, Set<String> completedLevelsIds, Set<String> playedLevelsIds) {
+        this.id = id;
+        this.name = name;
+        this.completedLevelsIds = completedLevelsIds == null ? new HashSet<>() : completedLevelsIds;
+        this.playedLevelsIds = playedLevelsIds == null ? new HashSet<>() : playedLevelsIds;
+    }
+
     public User(String id, String name) {
         this.id = id;
         this.name = name;
+        this.completedLevelsIds = new HashSet<>();
+        this.playedLevelsIds = new HashSet<>();
     }
 
     public String getId() {
@@ -24,4 +38,8 @@ public class User {
     public String getName() {
         return name;
     }
+
+    public Set<String> getCompletedLevelsIds() { return completedLevelsIds; }
+
+    public Set<String> getPlayedLevelsIds() { return playedLevelsIds; }
 }
