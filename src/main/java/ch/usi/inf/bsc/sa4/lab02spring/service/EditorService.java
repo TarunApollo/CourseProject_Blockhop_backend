cb60b0ba6840b21988300f9fcc2127e32bb75aa3
Nicola Perucchi <perucn@usi.ch>
2026-03-11 16:23:36 +0100

    #49: added javadoc for EditorService
    Co-authored-by Pavlo pavlo.harabadzhiiev@usi.ch

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
@@ -10,6 +10,11 @@ public class EditorService {
 
     private final LevelRepository levelRepository;
 
+    ///
+    /// Creates a new EditorService with the given repository.
+    /// @spec.requires levelRepository is not null.
+    /// @spec.effects creates an EditorService with the given levelRepository.
+    /// @param levelRepository the repository used to store levels.
     @Autowired
     public EditorService(LevelRepository levelRepository) {
         this.levelRepository = levelRepository;
