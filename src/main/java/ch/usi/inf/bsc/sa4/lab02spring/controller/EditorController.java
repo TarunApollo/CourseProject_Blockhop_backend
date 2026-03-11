2c4c9a2fbc188f3796eb10ddb03a0bcc14ee2d40
kvn1351 <kvn1351@gmail.com>
2026-03-11 15:33:19 +0100

    refactor EditorController to not have the validation checks. Moved that over to the service.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/EditorController.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/EditorController.java
@@ -1,29 +1,23 @@
 package ch.usi.inf.bsc.sa4.lab02spring.controller;
 
 import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.EditorLevelDTO;
-import ch.usi.inf.bsc.sa4.lab02spring.repository.LevelRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Level;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ch.usi.inf.bsc.sa4.lab02spring.service.EditorService;
-import ch.usi.inf.bsc.sa4.lab02spring.service.LevelService;
 import static ch.usi.inf.bsc.sa4.lab02spring.utils.AuthUtils.getUserIdFromAuth;
 
 @RestController
 @RequestMapping("/editor")
 public class EditorController {
-    private final LevelRepository levelRepository;
-    private final LevelService levelService;
     private final EditorService editorService;
-   
 
     @Autowired
     public EditorController(EditorService editorService) {
-                this.editorService = editorService;
-        }
+        this.editorService = editorService;
+    }
 
     /// Edits a tile in the world layer of an unpublished level.
     ///
@@ -31,30 +25,21 @@ public class EditorController {
     /// - If gid > 0 and the position is empty, a tile is added
     /// - If gid > 0 and the position already contains a tile, that tile is replaced
     /// - If gid == 0, the tile at that position is removed; if no tile exists, the operation is a no-op
-      ///
-  /// Allows creator to edit unpublished level
-  /// @param authentication abstract token for authentication
-  /// @param levelId id of the level being changed
-  /// @param dto data tranfer object holding the data to be updated.
-  /// @return a 200 if user is authenticated,is the creator, the level is unpublished and levels exists. Otherwise a 404 if level doesn't exist and a 401 if user not authenticated, the level is published, or is not the creator
-  /// 
-  
-
+    ///
+    /// Security checks are delegated to the EditorService, which enforces:
+    /// - Only the level creator can edit
+    /// - Only unpublished levels can be edited
+    /// - Coordinates must remain within level bounds (0 to width-1, 0 to height-1)
+    ///
+    /// @param authentication authentication token for the current user
+    /// @param levelId id of the level to edit
+    /// @param dto contains the target position and gid to apply
+    /// @return 200 OK with the updated level, 401 if unauthorized, 403 if the level is published,
+    ///         404 if level not found, 400 if coordinates are out of bounds
     @PutMapping("/{levelId}")
-  public ResponseEntity<Level> editLevel(Authentication authentication, @PathVariable String levelId, @RequestBody EditorLevelDTO dto){
-    String userId = getUserIdFromAuth(authentication);
-    String creatorId = this.levelService.creator(levelId);
-    Level level = this.levelService.getLevelbyId(levelId);
-    Boolean isPublished = this.levelService.published(level);
-    //
-    // also, you dont have to worry about any of this logic here, 
-    // just replicate whatever I did in for the level controller 
-    //
-    // && should be ||, but you can remove this as im checking this already
-    if(creatorId.equals(userId) == false && !isPublished){
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    } 
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // will be different but i will update it with the editorService methods
-
-}
+    public ResponseEntity<Level> editLevel(Authentication authentication, @PathVariable String levelId, @RequestBody EditorLevelDTO dto) {
+        String userId = getUserIdFromAuth(authentication);
+        // All validation (creator check, published check, bounds check) is handled by EditorService
+        return ResponseEntity.ok(this.editorService.editWorldLayerTile(userId, levelId, dto));
+    }
 }
