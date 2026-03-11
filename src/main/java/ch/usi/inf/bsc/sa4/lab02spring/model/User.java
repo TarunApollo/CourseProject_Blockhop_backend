c863a02bd06f94780ab04e974af901d0b4d5b632
Nicola Perucchi <perucn@usi.ch>
2026-03-11 16:19:49 +0100

    #48: small fix in javadoc for user model
    Co-authored-by Pavlo pavlo.harabadzhiiev@usi.ch

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/User.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/User.java
@@ -21,6 +21,8 @@ public class User {
 
     /// Creates a new User with no levels played or completed.
     /// @spec.requires id and name are not null.
+    /// @spec.effects creates a User with the given id and name,
+    ///               with levelsPlayed and levelsCompleted initialized as empty sets.
     /// @param id the unique identifier of the user (switchEduId).
     /// @param name the display name of the user.
     public User(String id, String name) {
@@ -28,11 +30,12 @@ public class User {
         this.name = name;
     }
 
-    ///
+    /// Creates a new User with possibly some levels played or completed.
     /// @spec.requires id, name, levelsPlayed, levelsCompleted are not null
+    /// @spec.effects creates a User with the given id, name, levelsPlayed, and levelsCompleted.
     /// @param id the unique identifier of the user (switchEduId)
     /// @param name the game name of the user
-    /// @param levelsPlayed the set of games plaxyed by the user
+    /// @param levelsPlayed the set of games played by the user
     /// @param levelsCompleted the set of levels completed by the user
     @PersistenceCreator
     public User(String id, String name, Set<Level> levelsPlayed, Set<Level> levelsCompleted) {
