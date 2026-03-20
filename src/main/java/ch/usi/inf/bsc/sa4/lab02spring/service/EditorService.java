b0161bd54f62a7b3074ac390aae7f512801fb7af
kvn1351 <kvn1351@gmail.com>
2026-03-20 19:10:15 +0100

    Separated the concerns of GameObject creation in the EditorService by creating a factory for them. However, the current way of creating the objects isn't ok.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/EditorService.java
@@ -9,18 +9,9 @@ import org.springframework.stereotype.Service;
 
 import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.EditorLevelDTO;
 import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.UpdateWorldLayerDTO;
-import ch.usi.inf.bsc.sa4.lab02spring.model.Box;
-import ch.usi.inf.bsc.sa4.lab02spring.model.Coin;
-import ch.usi.inf.bsc.sa4.lab02spring.model.Content;
-import ch.usi.inf.bsc.sa4.lab02spring.model.Decoration;
-import ch.usi.inf.bsc.sa4.lab02spring.model.ExitDoor;
-import ch.usi.inf.bsc.sa4.lab02spring.model.GameObject;
 import ch.usi.inf.bsc.sa4.lab02spring.model.GroundTileId;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Level;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Position;
-import ch.usi.inf.bsc.sa4.lab02spring.model.Slime;
-import ch.usi.inf.bsc.sa4.lab02spring.model.Snail;
-import ch.usi.inf.bsc.sa4.lab02spring.model.StartFlag;
 import ch.usi.inf.bsc.sa4.lab02spring.repository.LevelRepository;
 import ch.usi.inf.bsc.sa4.lab02spring.utils.LevelPublishedException;
 
@@ -29,14 +20,14 @@ import ch.usi.inf.bsc.sa4.lab02spring.utils.LevelPublishedException;
 public class EditorService {
 
     private final LevelRepository levelRepository;
-
-
     private final TileSetService tileSetService;
+    private final GameObjectFactory gameObjectFactory;
 
     @Autowired
-    public EditorService(LevelRepository levelRepository, TileSetService tileSetService ) {
+    public EditorService(LevelRepository levelRepository, TileSetService tileSetService, GameObjectFactory gameObjectFactory) {
         this.levelRepository = levelRepository;
         this.tileSetService = tileSetService;
+        this.gameObjectFactory = gameObjectFactory;
     }
 
     /// Edits a single tile in the world layer of a level.
@@ -126,26 +117,8 @@ public class EditorService {
             if (level.getObjectLayer().containsKey(dto.position())) {
                 throw new IllegalArgumentException("Tile already has an object");
             }
-            level.putObjectLayer(dto.position(), createGameObject(dto.gid(), dto.position()));
+            level.putObjectLayer(dto.position(), gameObjectFactory.createGameObject(dto.gid(), dto.position()));
         }
         return levelRepository.save(level);
-    }
-
-    private GameObject createGameObject(int gid, Position pos) {
-        String type = tileSetService.getObjectTileType(gid);
-        return switch (type) {
-            case "Decoration" -> new Decoration(gid, pos);
-            case "Enemy_Slime_Normal" -> new Slime(gid, pos);
-            case "Enemy_Snail" -> new Snail(gid, pos, false);
-            case "Box", "BoxDouble" -> new Box(gid, pos, new Content.NoContent() );
-            case "Start_Flag", "Start_Flag_B" -> new StartFlag(gid, pos);
-            case "Door_Closed", "Door_Open" -> new ExitDoor(gid, pos);
-            case "Item_Coin_Gold", "Item_Coin_Gold_Side" -> new Coin(gid, pos, 100);
-            case "Item_Coin_Silver", "Item_Coin_Silver_Side" -> new Coin(gid, pos, 25);
-            case "Item_Coin_Bronze", "Item_Coin_Bronze_Side" -> new Coin(gid, pos, 5);
-            case "Item_Shell" -> new Snail(gid, pos, true);
-            case "ExclamationMark" -> new Decoration(gid, pos);
-            default -> throw new IllegalArgumentException("Unknown object type: " + type);
-        };
     }
 }
