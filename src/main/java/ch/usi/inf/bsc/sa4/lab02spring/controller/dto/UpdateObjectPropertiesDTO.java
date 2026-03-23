daa6eb57bb6d29cc8b8642bd3b4e988e040d66d3
Tarun Deepak <tarundeepak2005@gmail.com>
2026-03-23 21:47:49 +0100

    Created new DTO to be able to manipulate ContentType of boxes.

---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/UpdateObjectPropertiesDTO.java
@@ -0,0 +1,6 @@
+package ch.usi.inf.bsc.sa4.lab02spring.controller.dto;
+
+import ch.usi.inf.bsc.sa4.lab02spring.model.Position;
+
+
+public record UpdateObjectPropertiesDTO(Position position , String contentType) {}
