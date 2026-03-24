8b52e95fe09971c63a2ee850affc2bde1f812ada
jeff <martijeffre@gmail.com>
2026-03-25 00:44:18 +0100

    4.1 Setup of PlayLevelDTO

---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/PlayLevelDTO.java
@@ -0,0 +1,7 @@
+package ch.usi.inf.bsc.sa4.lab02spring.controller.dto;
+import ch.usi.inf.bsc.sa4.lab02spring.model.Level;
+import ch.usi.inf.bsc.sa4.lab02spring.model.User;
+
+public record PlayLevelDTO(Level level, User user) {
+
+}
