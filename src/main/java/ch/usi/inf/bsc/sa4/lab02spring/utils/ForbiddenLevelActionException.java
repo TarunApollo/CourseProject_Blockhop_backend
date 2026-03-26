b872682c39d2fdea5cc04d4bcd63a938472066b0
Alessandro <alessandro.tognoni@usi.ch>
2026-03-26 22:26:01 +0100

    Created a ForbiddenLevelActionException.java to handle non allowed actions on levels.

---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/utils/ForbiddenLevelActionException.java
@@ -0,0 +1,9 @@
+package ch.usi.inf.bsc.sa4.lab02spring.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "This action is not allowed on this level.")
+public class ForbiddenLevelActionException extends RuntimeException {
+    public ForbiddenLevelActionException(String message) { super(message); }
+}
