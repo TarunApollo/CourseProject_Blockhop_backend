89cbdc81f75fdba6a0d7338a5b6c1ef84069fecd
Chengyu Yu <nightnight2024@gmail.com>
2026-03-26 17:11:17 +0100

    feat:Add aggregation for counting distinct levels/completed levels played by user.

---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/repository/AttemptStatisticsRepositoryImpl.java
@@ -0,0 +1,49 @@
+package ch.usi.inf.bsc.sa4.lab02spring.repository;
+
+import ch.usi.inf.bsc.sa4.lab02spring.model.User;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AttemptStatisticsRepositoryImpl implements AttemptStatisticsRepository {
+    private final MongoTemplate mongoTemplate;
+
+    public AttemptStatisticsRepositoryImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public long countDistinctPlayedLevelsByUser(User user) {
+        return countDistinctLevelsByUser(user, false);
+    }
+
+    @Override
+    public long countDistinctCompletedLevelsByUser(User user) {
+        return countDistinctLevelsByUser(user, true);
+    }
+
+    private long countDistinctLevelsByUser(User user, boolean completedOnly) {
+        Criteria criteria = Criteria.where("user.$id").is(user.getId());
+        if (completedOnly) {
+            criteria = criteria.and("completed").is(true);
+        }
+
+        Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.match(criteria),
+                Aggregation.group("level.$id"),
+                Aggregation.count().as("count")
+        );
+
+        Document result = mongoTemplate
+                .aggregate(aggregation, "attempts", Document.class)
+                .getUniqueMappedResult();
+
+        if(result == null)return 0;
+        Number count = (Number) result.get("count");
+        return count == null ? 0 : count.longValue();
+
+    }
+}
