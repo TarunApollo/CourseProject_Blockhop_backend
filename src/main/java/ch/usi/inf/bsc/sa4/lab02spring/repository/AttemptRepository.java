7586dcf36658a1ac3058b13b32aa25ba7dc6adcf
Chengyu Yu <nightnight2024@gmail.com>
2026-03-26 17:13:00 +0100

    Refactor:use repo aggregation in attemp stats.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/repository/AttemptRepository.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/repository/AttemptRepository.java
@@ -1,14 +1,14 @@
 package ch.usi.inf.bsc.sa4.lab02spring.repository;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Attempt;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Level;
-import ch.usi.inf.bsc.sa4.lab02spring.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.ZonedDateTime;
 
 @Repository
-public interface AttemptRepository extends MongoRepository<Attempt, String> {
-    List<Attempt> findByUser(User user);
-    List<Attempt> findByUserAndCompletedTrue(User user);
+public interface AttemptRepository extends MongoRepository<Attempt, String>,AttemptStatisticsRepository {
+    long countByLevel(Level level);
+    long countByLevelAndCompletedTrue(Level level);
+    long countByLevelAndTimestampAfter(Level level, ZonedDateTime after);
 }
