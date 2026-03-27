b9d0a2e1c62903a5a73cbe18e7d6dc2b98c7af7e
kvn1351 <kvn1351@gmail.com>
2026-03-27 01:44:48 +0100

    Revert "Add TODO comments to validate non-null properties for some DTOs"
    As we're not supposed to care about null apparently
    
    This reverts commit 4f2012ef865042c501bab6c2784bae14269246d3.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/EditorLevelDTO.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/EditorLevelDTO.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Content;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Position;
 
-// TODO: Add compact constructor to validate non-null position
 public record EditorLevelDTO(Position position, int gid, Content content) {
     
     @JsonCreator
--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/UpdateObjectLayerDTO.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/UpdateObjectLayerDTO.java
@@ -2,5 +2,7 @@ package ch.usi.inf.bsc.sa4.lab02spring.controller.dto;
 
 import java.util.List;
 
-// TODO: Add compact constructor to validate non-null and non-empty objects list
-public record UpdateObjectLayerDTO(List<EditorLevelDTO> objects) {}
+/// For batch operations on object layer.
+public record UpdateObjectLayerDTO(
+    List<EditorLevelDTO> objects
+) {}
