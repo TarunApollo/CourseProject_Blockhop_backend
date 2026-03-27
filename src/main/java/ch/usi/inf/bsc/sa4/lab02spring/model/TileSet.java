d8c9b41a174bc13f6bdd52279e03be6b57f7340f
kvn1351 <kvn1351@gmail.com>
2026-03-27 02:17:33 +0100

    Added JsonIgnoreProperties annotation to the TileSet to ignore unknown fields from the tileset json

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/TileSet.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/model/TileSet.java
@@ -2,13 +2,17 @@ package ch.usi.inf.bsc.sa4.lab02spring.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /// Record to parse only the essential tileset JSON data.
 /// Jackson ignores extra fields (columns, image, properties, objectgroup, etc.)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record TileSet(
     int firstgid,
     List<TileData> tiles
 ) {
     /// Represents minimal tile data: id (relative) and type.
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record TileData(
         int id,
         String type
