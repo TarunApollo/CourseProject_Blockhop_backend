facb5e8be4f411e0b840a8b410575b1554d3e06c
Chengyu Yu <nightnight2024@gmail.com>
2026-04-14 18:43:05 +0200

    fix:Add patch in white list of security config.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/configuration/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);  // required for session cookies
 
