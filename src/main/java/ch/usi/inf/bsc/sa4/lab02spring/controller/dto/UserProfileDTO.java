a673746bffa082818c9b9c3dd1460a1a359a3266
Nicola Perucchi <perucn@usi.ch>
2026-04-19 19:07:00 +0200

    #94 include statistics for created levels

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/UserProfileDTO.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/controller/dto/UserProfileDTO.java
@@ -8,14 +8,14 @@ public record UserProfileDTO(
         String name,
         long playedLevelsCount,
         long completedLevelsCount,
-        List<LevelDTO> createdLevels // List is better than Set because it preserves order for the frontend
+        List<CreatedLevelProfileDTO> createdLevels // List is better than Set because it preserves order for the frontend
 ) {
     /// Constructs a UserProfileDTO from the given User entity and statistics.
     /// @param user the user whose profile to represent
     /// @param playedLevelsCount the number of distinct levels the user has played
     /// @param completedLevelsCount the number of distinct levels the user has completed
     /// @param createdLevels the list of levels created by the user as LevelDTOs
-    public UserProfileDTO(User user, long playedLevelsCount, long completedLevelsCount, List<LevelDTO> createdLevels) {
+    public UserProfileDTO(User user, long playedLevelsCount, long completedLevelsCount, List<CreatedLevelProfileDTO> createdLevels) {
         this(
                 user.getName(),
                 playedLevelsCount,
