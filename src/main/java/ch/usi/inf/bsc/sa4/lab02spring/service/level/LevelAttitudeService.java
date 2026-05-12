e150d9c5c111acc744f49417469f5ce147e54268
Theodoros <theodoros.thomas@usi.ch>
2026-05-12 16:34:24 +0200

    #107: modified setAttitude() and deleteAttitude() to use findByLevelAndUser() and deleteByLevelAndUser() since each user can have at most one attitude per level.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/level/LevelAttitudeService.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/service/level/LevelAttitudeService.java
@@ -9,10 +9,10 @@ import ch.usi.inf.bsc.sa4.lab02spring.repository.LevelRepository;
 import ch.usi.inf.bsc.sa4.lab02spring.service.UserService;
 import ch.usi.inf.bsc.sa4.lab02spring.utils.LevelNotFoundException;
 import ch.usi.inf.bsc.sa4.lab02spring.utils.UserNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 /// Provides operations for managing level attitudes (likes and dislikes).
@@ -39,35 +39,32 @@ public class LevelAttitudeService {
         this.userService = userService;
     }
 
-    /// Sets or updates a user's attitude towards a level.
-    /// If the user already has an attitude for this level, it will be updated;
-    /// otherwise, a new attitude record is created.
+    /// Sets or updates a user's attitude towards a level. If the user already has
+    /// an attitude for this level, it will be updated, otherwise, a new attitude
+    /// record is created.
     ///
     /// @spec.requires userId and levelId are not null.
-    /// @spec.effects creates a new LevelAttitude or updates an existing one in the repository.
+    /// @spec.effects creates a new LevelAttitude or updates an existing one 
+    ///               in the repository.
     /// @param userId the unique identifier of the user
     /// @param levelId the unique identifier of the level
     /// @param attitude the attitude type (like or dislike)
     /// @return the created or updated LevelAttitude
     /// @throws UserNotFoundException if the user does not exist
     /// @throws LevelNotFoundException if the level does not exist
-    public LevelAttitude setAttitude(final String userId, final String levelId, final LevelAttitudeType attitude) {
+    public LevelAttitude setAttitude(final String userId, final String levelId, final LevelAttitudeType attitudeType) {
         final User user = this.userService.getById(userId).orElseThrow(UserNotFoundException::new);
         final Level level = this.levelRepository.findById(levelId).orElseThrow(LevelNotFoundException::new);
 
-        // Find existing attitude, if any
-        final List<LevelAttitude> existingAttitudes = this.attitudeRepository.findByLevel(level).stream()
-                .filter(a -> a.getUser().getId().equals(userId))
-                .toList();
+        // Find existing attitude by level and user (at most one per user-level pair)
+        final Optional<LevelAttitude> existing = this.attitudeRepository.findByLevelAndUser(level, user);
 
-        if (!existingAttitudes.isEmpty()) {
-            // Update existing attitude
-            final LevelAttitude existingAttitude = existingAttitudes.get(0);
-            existingAttitude.setAttitude(attitude);
-            return this.attitudeRepository.save(existingAttitude);
+        if (existing.isPresent()) {
+            final LevelAttitude att = existing.get();
+            att.setAttitude(attitudeType);
+            return this.attitudeRepository.save(att);
         } else {
-            // Create new attitude
-            final LevelAttitude newAttitude = new LevelAttitude(user, level, attitude);
+            final LevelAttitude newAttitude = new LevelAttitude(user, level, attitudeType);
             return this.attitudeRepository.save(newAttitude);
         }
     }
@@ -75,9 +72,11 @@ public class LevelAttitudeService {
     /// Deletes a user's attitude towards a level if it exists.
     ///
     /// @spec.requires userId and levelId are not null.
-    /// @spec.modifies removes the user's attitude record for the level from the repository if present.
-    /// @spec.effects deletes the attitude if found; does nothing if no attitude exists.
-    /// @param userId the unique identifier of the user
+    /// @spec.modifies removes the user's attitude record for the level from the
+    ///                repository if present.
+    /// @spec.effects deletes the attitude if found; does nothing if no attitude
+    ///               exists.
+    /// @param userId  the unique identifier of the user
     /// @param levelId the unique identifier of the level
     /// @throws UserNotFoundException if the user does not exist
     /// @throws LevelNotFoundException if the level does not exist
@@ -86,12 +85,7 @@ public class LevelAttitudeService {
         final Level level = this.levelRepository.findById(levelId).orElseThrow(LevelNotFoundException::new);
 
         // Find and delete the user's attitude for this level, if it exists
-        final Optional<LevelAttitude> attitude = this.attitudeRepository.findByLevel(level).stream()
-                .filter(a -> a.getUser().getId().equals(userId))
-                .findFirst();
-
-        if (attitude.isPresent()) {
-            this.attitudeRepository.deleteById(attitude.get().getId());
-        }
+        this.attitudeRepository.findByLevelAndUser(level, user)
+                .ifPresent(a -> this.attitudeRepository.deleteByLevelAndUser(level, user));
     }
 }
