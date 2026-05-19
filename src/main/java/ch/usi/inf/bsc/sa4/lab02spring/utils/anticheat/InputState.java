ade7fad2d7954872477d0947277e00fba17246a2
Alessandro Tognoni <alessandro.tognoni@usi.ch>
2026-05-20 01:46:22 +0200

    Addeed comments for explicitly declaring the modifier.

--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/utils/anticheat/InputState.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/utils/anticheat/InputState.java
@@ -5,33 +5,33 @@ import ch.usi.inf.bsc.sa4.lab02spring.controller.dto.InputFrameDTO;
 /// Button state for one frame.
 record InputState(boolean left, boolean right, boolean jump, boolean run) {
 
-    static InputState from(final InputFrameDTO frame) {
+    /* package */ static InputState from(final InputFrameDTO frame) {
         return new InputState(frame.left(), frame.right(), frame.jump(), frame.run());
     }
 
-    String canonical() {
+    /* package */ String canonical() {
         return "L" + bit(left)
                 + "R" + bit(right)
                 + "J" + bit(jump)
                 + "S" + bit(run);
     }
 
-    boolean isNeutral() {
+    /* package */ boolean isNeutral() {
         return !left && !right && !jump && !run;
     }
 
-    boolean isJumpOnly() {
+    /* package */ boolean isJumpOnly() {
         return !left && !right && jump && !run;
     }
 
-    boolean sameHorizontalDirection(final InputState other) {
+    /* package */ boolean sameHorizontalDirection(final InputState other) {
         return hasSingleHorizontalDirection()
                 && other.hasSingleHorizontalDirection()
                 && left == other.left()
                 && right == other.right();
     }
 
-    boolean oppositeHorizontalDirection(final InputState other) {
+    /* package */ boolean oppositeHorizontalDirection(final InputState other) {
         return hasSingleHorizontalDirection()
                 && other.hasSingleHorizontalDirection()
                 && left == other.right()
