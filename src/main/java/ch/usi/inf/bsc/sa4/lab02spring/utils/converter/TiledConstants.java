b7762a2bd0c842661760f5287e743416d914e425
Leon Jacopo Costa <leon.costa@usi.ch>
2026-05-22 20:11:57 +0200

    refactor: simplify tiled map export

---
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/utils/converter/TiledConstants.java
@@ -0,0 +1,16 @@
+package ch.usi.inf.bsc.sa4.lab02spring.utils.converter;
+
+/// Common constants used for Tiled JSON export.
+@SuppressWarnings("PMD.AtLeastOneConstructor")
+public final class TiledConstants {
+    public static final String KEY_NAME = "name";
+    public static final String KEY_TYPE = "type";
+    public static final String KEY_VISIBLE = "visible";
+    public static final String KEY_WIDTH = "width";
+    public static final String KEY_HEIGHT = "height";
+    public static final String KEY_VALUE = "value";
+
+    public static final String TYPE_STRING = "string";
+    public static final String TYPE_INT = "int";
+
+}
--- src/main/java/ch/usi/inf/bsc/sa4/lab02spring/utils/converter/TiledLayerMapper.java
+++ src/main/java/ch/usi/inf/bsc/sa4/lab02spring/utils/converter/TiledLayerMapper.java
@@ -1,42 +1,47 @@
 package ch.usi.inf.bsc.sa4.lab02spring.utils.converter;
 
+import static ch.usi.inf.bsc.sa4.lab02spring.utils.converter.TiledConstants.KEY_HEIGHT;
+import static ch.usi.inf.bsc.sa4.lab02spring.utils.converter.TiledConstants.KEY_NAME;
+import static ch.usi.inf.bsc.sa4.lab02spring.utils.converter.TiledConstants.KEY_TYPE;
+import static ch.usi.inf.bsc.sa4.lab02spring.utils.converter.TiledConstants.KEY_VISIBLE;
+import static ch.usi.inf.bsc.sa4.lab02spring.utils.converter.TiledConstants.KEY_WIDTH;
+import static ch.usi.inf.bsc.sa4.lab02spring.utils.converter.TiledConstants.TYPE_STRING;
+
 import ch.usi.inf.bsc.sa4.lab02spring.model.Box;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Content;
+import ch.usi.inf.bsc.sa4.lab02spring.model.Entry;
 import ch.usi.inf.bsc.sa4.lab02spring.model.GameObject;
 import ch.usi.inf.bsc.sa4.lab02spring.model.GroundObject;
 import ch.usi.inf.bsc.sa4.lab02spring.model.Position;
-import ch.usi.inf.bsc.sa4.lab02spring.service.TileSetService;
+import ch.usi.inf.bsc.sa4.lab02spring.service.TileCatalogService;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-/// Maps level layers into Tiled-compatible layer structures.
+/// Converts level layers to map data for the frontend.
+@SuppressWarnings("PMD.AtLeastOneConstructor")
 final class TiledLayerMapper {
-    /// Pixel size used for each exported tile.
+    /// Size of one tile in pixels.
     private static final int TILE_SIZE = 128;
 
-    /// JSON key for the Tiled `name` field.
-    private static final String KEY_NAME = "name";
-    /// JSON key for the Tiled `type` field.
-    private static final String KEY_TYPE = "type";
-    /// JSON key for the Tiled `visible` field.
-    private static final String KEY_VISIBLE = "visible";
-    /// JSON key for the Tiled `width` field.
-    private static final String KEY_WIDTH = "width";
-    /// JSON key for the Tiled `height` field.
-    private static final String KEY_HEIGHT = "height";
-
-    private TiledLayerMapper() {
-    }
-
-    /// Package-private helper that exports the world layer in Tiled format.
+    /// Builds the world layer with tile ids.
+    ///
+    /// @spec.requires worldLayer and tileCatalogService are not null.
+    /// @spec.modifies nothing.
+    /// @spec.effects builds the exported world layer from level tiles.
+    /// @param worldLayer         world tiles by grid position
+    /// @param width              level width in tiles
+    /// @param height             level height in tiles
+    /// @param tileCatalogService catalog service used by the converter
+    /// @return world layer data
     /* package */ static Map<String, Object> buildWorldLayer(
             final Map<Position, GroundObject> worldLayer,
             final int width,
-            final int height) {
-        final List<Integer> data = new ArrayList<>(Collections.nCopies(width * height, 0));
+            final int height,
+            final TileCatalogService tileCatalogService) {
+        final List<String> data = new ArrayList<>(Collections.nCopies(width * height, ""));
 
         for (final Map.Entry<Position, GroundObject> entry : worldLayer.entrySet()) {
             final Position pos = entry.getKey();
@@ -48,7 +53,7 @@ final class TiledLayerMapper {
             }
 
             final int idx = y * width + x;
-            data.set(idx, groundObject.gid());
+            data.set(idx, groundObject.tileId());
         }
 
         return Map.of(
@@ -64,14 +69,21 @@ final class TiledLayerMapper {
                 "data", data);
     }
 
-    /// Package-private helper that exports the object layer in Tiled format.
+    /// Builds the object layer with tile types from the catalog.
+    ///
+    /// @spec.requires objectLayer and tileCatalogService are not null.
+    /// @spec.modifies nothing.
+    /// @spec.effects builds the exported object layer from level objects.
+    /// @param objectLayer        objects by grid position
+    /// @param tileCatalogService catalog service used to resolve tile types
+    /// @return object layer data
     /* package */ static Map<String, Object> buildObjectLayer(
             final Map<Position, GameObject> objectLayer,
-            final TileSetService tileSetService) {
+            final TileCatalogService tileCatalogService) {
         final List<Map<String, Object>> objects = new ArrayList<>(objectLayer.size());
         int idCounter = 1;
         for (final GameObject gameObject : objectLayer.values()) {
-            objects.add(toTiledObject(gameObject, idCounter, tileSetService));
+            objects.add(toTiledObject(gameObject, idCounter, tileCatalogService));
             idCounter++;
         }
 
@@ -87,40 +99,61 @@ final class TiledLayerMapper {
                 "objects", objects);
     }
 
+    /// Converts one game object into frontend map data.
+    ///
+    /// @spec.requires gameObject and tileCatalogService are not null.
+    /// @spec.modifies nothing.
+    /// @spec.effects builds one exported object and resolves its type from the
+    ///               catalog.
+    /// @param gameObject         object to convert
+    /// @param id                 object id in the exported layer
+    /// @param tileCatalogService catalog service used to resolve tile type
+    /// @return object data
     private static Map<String, Object> toTiledObject(
             final GameObject gameObject,
             final int id,
-            final TileSetService tileSetService) {
+            final TileCatalogService tileCatalogService) {
         final Position pos = gameObject.pos();
         final int x = pos.x() * TILE_SIZE;
         final int y = (pos.y() + 1) * TILE_SIZE;
-        final String type = tileSetService.getObjectTileType(gameObject.gid());
+        final Entry catalogEntry = tileCatalogService.requireTile(gameObject.tileId());
 
-        final List<Map.Entry<String, Object>> entries = new ArrayList<>(List.of(
-                Map.entry("id", id),
-                Map.entry("gid", gameObject.gid()),
-                Map.entry("x", x),
-                Map.entry("y", y),
-                Map.entry(KEY_WIDTH, TILE_SIZE),
-                Map.entry(KEY_HEIGHT, TILE_SIZE),
-                Map.entry(KEY_VISIBLE, Boolean.TRUE),
-                Map.entry("rotation", 0),
-                Map.entry(KEY_NAME, ""),
-                Map.entry(KEY_TYPE, type)));
+        final List<Map.Entry<String, Object>> entries = new ArrayList<>();
+        entries.add(Map.entry("id", id));
+        entries.add(Map.entry("tileId", gameObject.tileId()));
+        entries.add(Map.entry("x", x));
+        entries.add(Map.entry("y", y));
+        entries.add(Map.entry(KEY_WIDTH, TILE_SIZE));
+        entries.add(Map.entry(KEY_HEIGHT, TILE_SIZE));
+        entries.add(Map.entry(KEY_VISIBLE, Boolean.TRUE));
+        entries.add(Map.entry("rotation", 0));
+        entries.add(Map.entry(KEY_NAME, ""));
+        entries.add(Map.entry(KEY_TYPE, catalogEntry.type()));
+
         if (gameObject instanceof Box box && box.content() instanceof Content.SomeContent) {
             entries.add(Map.entry("properties", buildBoxProperties(box)));
         }
-        return Map.ofEntries(entries.toArray(new Map.Entry[0]));
+
+        final Map.Entry<String, Object>[] entriesArray = entries.toArray(new Map.Entry[0]);
+        return Map.ofEntries(entriesArray);
     }
 
+    /// Builds the content property for boxes.
+    ///
+    /// @spec.requires box is not null.
+    /// @spec.modifies nothing.
+    /// @spec.effects builds exported properties for the box content.
+    /// @param box the box to read
+    /// @return box properties
     private static List<Map<String, Object>> buildBoxProperties(final Box box) {
         final List<Map<String, Object>> properties = new ArrayList<>();
         if (box.content() instanceof Content.SomeContent someContent) {
             properties.add(Map.of(
                     KEY_NAME, "Content",
-                    KEY_TYPE, "string",
+                    KEY_TYPE, TYPE_STRING,
                     "value", someContent.coinType().value()));
         }
         return properties;
     }
+
 }
